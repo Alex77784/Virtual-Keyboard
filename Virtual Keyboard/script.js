4b03d0f4daa51dd10695c945161b4403ff41d89d
Alex77784 <7509608@gmail.com>
2022-05-08 14:17:42 +0300

    feat:create backlight buttons

--- Virtual Keyboard/script.js
+++ Virtual Keyboard/script.js
@@ -1,22 +1,27 @@
-let keyboardArray = [
+let keyboardDefault = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'NumpadDecimal', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'
+];
+let keyboardFace = [
     '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
     '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650', 'Shift', 'Cntr', 'Win', 'Alt', '', 'Alt', '&#9668', '&#9660', '&#9658', 'Cntr'
 ];
-let keyboardArrayClass = [
-    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
-    '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'ShiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'ShiftRight', 'CntrLeft', 'Win', 'AltLeft', 'Space', 'AltRight', 'Left', 'Down', 'Right', 'CntrRight'
-];
+
 let capsLock = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'
 ];
 let shift = [
     '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'
 ];
+
 // screenBegin
-let body = document.body,
+let body = document.body,//*
     display = document.createElement('textarea');
 body.prepend(display);
 // screenEnd
+
+// title
+let title = document.createElement('h1');
+body.prepend(title);//*body
+title.innerHTML = 'Virtual Keyboard';
 
 // keyboardBegin
 let keyboardCreate = document.createElement('div');
@@ -43,45 +48,63 @@ let row1Class = row1.classList.add('row1'),
     row4Class = row4.classList.add('row4'),
     row5Class = row5.classList.add('row5');
 
-// createButton 
+// createButton with class
 for (let i = 0; i < 14; i++) {
-    const element = keyboardArray[i];
-    const elementClass = keyboardArrayClass[i];
+    const element = keyboardFace[i];
     let buttonRow1 = document.createElement('li');
+
     row1.append(buttonRow1);
     buttonRow1.innerHTML = element;
-    buttonRow1.classList.add('btn' + elementClass);
-}
+    buttonRow1.classList.add('btn' + element);
+};
+
 for (let i = 14; i < 29; i++) {
-    const element = keyboardArray[i];
-    const elementClass = keyboardArrayClass[i];
+    const element = keyboardFace[i];
+    const elementDefault = keyboardDefault[i];
     let buttonRow2 = document.createElement('li');
+
     row2.append(buttonRow2);
     buttonRow2.innerHTML = element;
-    buttonRow2.classList.add('btn' + elementClass);
-}
+    buttonRow2.classList.add('btn' + elementDefault)
+};
+
 for (let i = 29; i < 42; i++) {
-    const element = keyboardArray[i];
-    const elementClass = keyboardArrayClass[i];
+    const element = keyboardFace[i];
+    const elementDefault = keyboardDefault[i];
     let buttonRow3 = document.createElement('li');
+
     row3.append(buttonRow3);
     buttonRow3.innerHTML = element;
-    buttonRow3.classList.add('btn' + elementClass);
-}
+    buttonRow3.classList.add('btn' + elementDefault);
+};
+
 for (let i = 42; i < 55; i++) {
-    const element = keyboardArray[i];
-    const elementClass = keyboardArrayClass[i];
+    const element = keyboardFace[i];
+    const elementDefault = keyboardDefault[i];
     let buttonRow4 = document.createElement('li');
+
     row4.append(buttonRow4);
     buttonRow4.innerHTML = element;
-    buttonRow4.classList.add('btn' + elementClass);
-}
-for (let i = 55; i < keyboardArray.length; i++) {
-    const element = keyboardArray[i];
-    const elementClass = keyboardArrayClass[i];
+    buttonRow4.classList.add('btn' + elementDefault);
+};
+
+for (let i = 55; i < keyboardDefault.length; i++) {
+    const element = keyboardFace[i];
+    const elementDefault = keyboardDefault[i];
     let buttonRow5 = document.createElement('li');
+
     row5.append(buttonRow5);
     buttonRow5.innerHTML = element;
-    buttonRow5.classList.add('btn' + elementClass);
-}
+    buttonRow5.classList.add('btn' + elementDefault);
+};
+
+// backlight buttons
+document.onkeydown = (event) => {
+    document.querySelector('.btn' + event.code).
+        classList.add('active');
+};
+document.onkeyup = (event) => {
+    document.querySelector('.btn' + event.code).
+        classList.remove('active');
+};
 
